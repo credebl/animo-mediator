0a62cf0d402558563cbb6de02a301bbb7518c438
Tipu_Singh <tipu.singh@ayanworks.com>
2024-12-03 18:04:23 +0530

    dev local configs
    Signed-off-by: Tipu_Singh <tipu.singh@ayanworks.com>

--- dev.ts
+++ dev.ts
@@ -10,19 +10,19 @@ const port = 3000
  * the index file.
  */
 // TODO: have to add auth token now to use ngrok check this later
-void connect({
-  port,
-}).then((app) => {
-  // eslint-disable-next-line no-console
-  console.log('Got ngrok url:', app.url())
-  const url = app.url()
-  // const url = "http://192.168.1.71:3000"
+// void connect({
+//   port,
+// }).then((app) => {
+//   // eslint-disable-next-line no-console
+//   console.log('Got ngrok url:', app.url())
+//   const url = app.url()
 
-  process.env.NODE_ENV = 'development'
-  process.env.AGENT_PORT = `${port}`
-  // process.env.AGENT_ENDPOINTS = `${url}`
-  process.env.AGENT_ENDPOINTS = `${url},${url?.replace('http', 'ws')}`
-  process.env.SHORTENER_BASE_URL = `${url}/s`
+const url = 'http://localhost:3000'
 
-  require('./src/index')
-})
+process.env.NODE_ENV = 'development'
+process.env.AGENT_PORT = `${port}`
+process.env.AGENT_ENDPOINTS = `${url},${url?.replace('http', 'ws')}`
+process.env.SHORTENER_BASE_URL = `${url}/s`
+
+require('./src/index')
+// })
