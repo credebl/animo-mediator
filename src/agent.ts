4377f481cfd24dd1351de38e6e6164b949183533
Krishna Waske <krishna.waske@ayanworks.com>
2024-12-24 10:58:06 +0530

    chore: update MFS to Queue and LiveMode
    Signed-off-by: Krishna Waske <krishna.waske@ayanworks.com>

--- src/agent.ts
+++ src/agent.ts
@@ -42,7 +42,7 @@ function createModules() {
     }),
     mediator: new MediatorModule({
       autoAcceptMediationRequests: true,
-      messageForwardingStrategy: MessageForwardingStrategy.DirectDelivery,
+      messageForwardingStrategy: MessageForwardingStrategy.QueueAndLiveModeDelivery,
     }),
     askar: new AskarModule({
       ariesAskar,
